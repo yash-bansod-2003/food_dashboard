e6e6d2b3bbb48ab9a96310ce5d811b4316aa11ad
= <yashbansodgit@gmail.com>
2024-12-06 00:17:10 +0530

    feat: implemented card auth login

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router";
 import HomePage from "./pages/home";
+import LoginPage from "./pages/auth/login";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/auth/login" element={<LoginPage />} />
     </Routes>
   );
 }
